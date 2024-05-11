9b9fd676766bd63a7db3637feedc16daa2029562
Kassy <78169097+Kassy2048@users.noreply.github.com>
2024-05-11 17:50:54 +0200

    Ignore unknown characters

--- src/regalia/js/GameConditions.js
+++ src/regalia/js/GameConditions.js
@@ -252,6 +252,7 @@ var GameConditions = {
             }
             case "CT_Character_In_Room": {
                 var tempchar = Finder.character(step2);
+                if(tempchar == null) return false;
                 if (step3 == CurrentRoomGuid) {
                     var currentroom = Finder.room(TheGame.Player.CurrentRoom);
                     return tempchar.CurrentRoom == currentroom.UniqueID;
@@ -261,6 +262,7 @@ var GameConditions = {
             }
             case "CT_Character_In_RoomGroup": {
                 var tempchar = Finder.character(step2);
+                if(tempchar == null) return false;
                 var temproom = Finder.room(tempchar.CurrentRoom);
                 if (temproom != null) {
                     if (temproom.Group == step3)
